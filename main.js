c52cd294e70eac429d5fe87cff7eaa007d0df475
baskalmoment <123458726+BaskalM0ment@users.noreply.github.com>
2026-04-21 23:22:00 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,25 +1,33 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
-
-app.commandLine.appendSwitch('force-dark-mode');
-app.commandLine.appendSwitch('enable-features', 'WebContentsForceDark');
+const { app, BrowserWindow } = require("electron");
 
 function createWindow() {
   const win = new BrowserWindow({
     width: 1200,
     height: 800,
+    frame: false,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
-      webviewTag: true,
-      contextIsolation: true
+      nodeIntegration: true,
+      contextIsolation: false,
+      webviewTag: true   // REQUIRED
     }
   });
 
-  win.loadFile('index.html');
+  win.loadFile("index.html");
 }
+const { ipcMain } = require("electron");
+
+ipcMain.on("minimize", (e) => {
+  BrowserWindow.fromWebContents(e.sender).minimize();
+});
+
+ipcMain.on("maximize", (e) => {
+  const win = BrowserWindow.fromWebContents(e.sender);
+  if (win.isMaximized()) win.unmaximize();
+  else win.maximize();
+});
+
+ipcMain.on("close", (e) => {
+  BrowserWindow.fromWebContents(e.sender).close();
+});
 
 app.whenReady().then(createWindow);
-
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit();
-});
